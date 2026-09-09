bde22a4a2f2219e6c5c67aeddab925ebe5e027f8
Alao Oluwagbenga <gbenga1717@gmail.com>
2026-09-09 14:51:24 -0700

    trigger deploy

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:3000',          // local development
   'http://localhost:5173',          // Vite default
-  'https://flowershopfrontend-h6mlfaj93-gbengs1.vercel.app',   // production (no /login)
+  'https://flowershopfrontend-61d7ljzog-gbengs1.vercel.app/,   // production (no /login)
 ];
 
 app.use(cors({
