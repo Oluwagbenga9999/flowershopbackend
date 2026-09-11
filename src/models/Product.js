5e64c0461bb42639cd3e92a18ca788cd20d8d17f
Alao Oluwagbenga <gbenga1717@gmail.com>
2026-09-11 12:12:40 -0700

    integrated cloudinary

--- src/models/Product.js
+++ src/models/Product.js
@@ -5,7 +5,7 @@ const productSchema = new mongoose.Schema({
     type: { type: String, enum: ["flower", "planter"], required: true },
     price: { type: Number, required: true },
     description: { type: String },
-    image: { type: String }, // URL for now, Cloudinary later
+    images: { type: [String], default: [] }, // array of Cloudinary URLs
     stock: { type: Number, default: 0 },
 }, { timestamps: true });
 
